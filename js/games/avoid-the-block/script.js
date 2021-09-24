6582f48381df9041712984dc84b2c3f7993f6ba2
Augustin Holguin <augustin.holguin@gmail.com>
2021-09-24 17:42:36 +0200

    started logic to move mini dino along x axis

--- js/games/avoid-the-block/script.js
+++ js/games/avoid-the-block/script.js
@@ -3,16 +3,27 @@ const app = {
     trex: undefined,
     init: () => {
         app.jumpListener();
+        app.moveListener();
         app.dino = document.getElementById('player');
         app.trex = document.getElementById('t-rex');
         app.gameOver();
     },
+    // listen to the user clicking on jump button
     jumpListener: () => {
         const btn = document.getElementById('jump');
         btn.addEventListener('click', () => {
             app.jump();
         })
     },
+    // listen to keyboard arrow events
+    moveListener: () => {
+        document.addEventListener('keyup', (evt) => {
+            // console.log(evt);
+            const action = evt.code;
+            app.moveDino(action);
+        })
+    },
+    // function to make mini dino jump
     jump: () => {
         if (app.dino.className === 'jump') {
             return;
@@ -23,6 +34,12 @@ const app = {
             }, 1200)
         }
     },
+    // function to make mini dino move along the x axis
+    moveDino: (action) => {
+        console.log(action);
+    },
+    // function to check if mini dino and trex have entered in collision
+    // which results in ending the game
     gameOver: () => {
         setInterval(() => {
             const dinoTop = app.dino.computedStyleMap().get('top').value;
