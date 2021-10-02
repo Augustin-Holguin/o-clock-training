411204c471cb4fbfed4d212998e8c12a995424c8
Augustin Holguin <augustin.holguin@gmail.com>
2021-10-02 17:42:36 +0200

    ripe-tomatoes: finished separation of router functions in controller.js

--- js/ripe-tomatoes/app/controller.js
+++ js/ripe-tomatoes/app/controller.js
@@ -9,7 +9,35 @@ const controller = {
         }
         // filter unique alphabetically sorted values with Set constructor and spread operator
         const uniqueGenre = [...new Set(variousGenre.sort())];
+        
         return uniqueGenre;
+    },
+    // method to handle movie search
+    searchMovies: (req, res) => {
+        const searchQuery = req.query.q;
+        const movies = req.app.locals.movies;
+
+        const searchMatch = movies.filter((movie) => {
+            if (movie.title.toLowerCase().includes(searchQuery.toLowerCase())) {
+                return true;
+            }   
+        });
+
+        return searchMatch;
+    },
+    // method to find all movies which have the same genre
+    sameGenre: (req, res) => {
+        const genre = req.params.genre;
+        const movies = req.app.locals.movies;
+        const uniqueGenre = controller.variousGenre(req, res);
+
+        const sameGenreMovies = movies.filter((movie) => {
+            if (movie.genre.includes(genre)) {
+                return true;
+            }
+        });
+
+        return sameGenreMovies;
     }
 }
 
--- js/ripe-tomatoes/router.js
+++ js/ripe-tomatoes/router.js
@@ -12,12 +12,7 @@ router.get('/', (req, res) => {
 router.get('/search', (req, res, next) => {
     const searchQuery = req.query.q;
     const movies = req.app.locals.movies;
-
-    const searchMatch = movies.filter((movie) => {
-        if (movie.title.toLowerCase().includes(searchQuery.toLowerCase())) {
-            return true;
-        }
-    });
+    const searchMatch = controller.searchMovies(req, res);
 
     res.render('searchResult', { searchMatch, searchQuery });
 })
@@ -26,12 +21,7 @@ router.get('/genre/:genre', (req, res) => {
     const genre = req.params.genre;
     const movies = req.app.locals.movies;
     const uniqueGenre = controller.variousGenre(req, res);
-
-    const sameGenreMovies = movies.filter((movie) => {
-        if (movie.genre.includes(genre)) {
-            return true;
-        }
-    });
+    const sameGenreMovies = controller.sameGenre(req, res);
 
     res.render('genre', { sameGenreMovies, genre, uniqueGenre });
 })
